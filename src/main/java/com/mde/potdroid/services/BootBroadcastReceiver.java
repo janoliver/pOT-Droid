702565e1e41f0d5a032f5b3f43266ae7e952e298
Jan Oliver Oelerich <janoliver@oelerich.org>
2016-03-06 16:23:00 +0100

    Message Polling Service is not started now if polling is disabled.

--- src/main/java/com/mde/potdroid/services/BootBroadcastReceiver.java
+++ src/main/java/com/mde/potdroid/services/BootBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.mde.potdroid.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import com.mde.potdroid.helpers.SettingsWrapper;
 
 /**
  * A Broadcast Receiver that should be triggered after Boot. It starts the AlarmManager Service
@@ -12,6 +13,12 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        SettingsWrapper settings = new SettingsWrapper(context);
+
+        // return, if polling is disabled
+        if (settings.pollMessagesInterval().equals(0))
+            return;
+
         Intent startServiceIntent = new Intent(context, MessagePollingService.class);
         context.startService(startServiceIntent);
     }
